7c8a2bc9df3a417804316063a08ccbde7d436e97
c0nnor263 <36234732+c0nnor263@users.noreply.github.com>
2023-02-28 04:38:14 +0200

    Initial commit

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,6 +20,7 @@ plugins {
 }
 
 
+
 fun isNonStable(version: String): Boolean {
     val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { version.uppercase().contains(it) }
     val regex = "^[0-9,.v-]+(-r)?$".toRegex()
